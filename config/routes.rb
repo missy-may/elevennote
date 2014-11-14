7aa8e5968ed4520c58768abd1d2a369bdf25a243
Missy May <missysuemay@gmail.com>
2014-11-13 21:22:12 -0500

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   resources :users, only: :create
+  resources :notes, except: :edit
   get 'sign_up' => 'users#new', as: :sign_up
-  resources :sessions, only: [:create]
+  resources :sessions, only: :create
   delete 'logout' => 'sessions#destroy', as: :logout
-  get    'login' => 'sessions#new',      as: :login
-  resources :notes
-
+  get    'login'  => 'sessions#new',     as: :login
 end
